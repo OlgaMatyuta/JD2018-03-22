ecb8af9ba3dbc662dfd57c41df32bd6222abed30
Olga_Matyuta <tiefatem@gmail.com>
2018-05-12 12:00:41 +0300

    jd01...02_05 tmp

--- src/by/it/matyuta/jd02_05/TaskA.java
+++ src/by/it/matyuta/jd02_05/TaskA.java
@@ -5,10 +5,12 @@ import java.util.Locale;
 public class TaskA {
     public static void main(String[] args) {
         ResourceManager rm = ResourceManager.INSTANCE;
-        if(args.length<2)
-                rm.setLocale(Locale.US);
+        if (args.length<2)
+            rm.setLocale(Locale.US);
         else
-        rm.setLocale(new Locale("be","BY"));
-       // System.out.println(rm.get(Messeges.WELCOME));
+            rm.setLocale(new Locale(args[0],args[1]));
+        System.out.println(rm.get(Messages.WELCOME));
+        System.out.println(rm.get(Messages.HOWDOYOUDO));
+        System.out.println(rm.get(Messages.NAME));
     }
 }
